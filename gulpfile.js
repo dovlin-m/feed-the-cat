3ed0646b12c69ca172f77ecc7ad48e4c467e530a
dovlin_m <dovlin.m@gmail.com>
2022-06-24 16:15:22 +0300

    added gulp build

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const { src, dest, parallel } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const pug = require('gulp-pug');
+
+const buildSass = () => {
+  console.log('Компиляция SASS');
+
+  return src('scss/app.scss')
+    .pipe(sass())
+    .pipe(dest('css/'));
+}
+
+const buildPug = () => {
+  console.log('Компиляция Pug');
+
+  return src("pug/index.pug")
+    .pipe(pug({pretty: true}))
+    .pipe(pug())
+    .pipe(dest('html/'));
+}
+
+exports.build = parallel(buildSass, buildPug);
